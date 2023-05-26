80d8721e71b435331d7bcd2d0f778162ee65484f
Saiful082 <smsaifulislam082@.com>
2023-05-27 02:58:08 +0600

    login page design with css

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDTuPBumlh9PKBb6uuE1jirundMbFvFJeo",
+  authDomain: "ema-john-simple-255a3.firebaseapp.com",
+  projectId: "ema-john-simple-255a3",
+  storageBucket: "ema-john-simple-255a3.appspot.com",
+  messagingSenderId: "203755486566",
+  appId: "1:203755486566:web:30f456c496caa84dfa9ca7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
