5bbc468be7df7337cc15690d4cd1663f88cb23af
Saiful082 <smsaifulislam082@.com>
2023-05-29 01:23:51 +0600

    hosting firebase and deplay firebase

--- src/components/Shipping/Shipping.js
+++ src/components/Shipping/Shipping.js
@@ -4,6 +4,8 @@ const Shipping = () => {
     return (
         <div>
             <h2>Shipping</h2>
+            <p>On the Way</p>
+            <p><small>Coming Soon.....</small></p>
         </div>
     );
 };
